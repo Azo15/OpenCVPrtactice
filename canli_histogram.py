4eac96127672dc807f755c946f60951bf8eda721
Azo Ismail <ismailazo260@gmail.com>
2026-03-02 13:56:32 +0300

    style: Remove unnecessary blank line in canli_histogram.py

--- canli_histogram.py
+++ canli_histogram.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 cap = cv2.VideoCapture(0)
 
 print("Histogram uygulamasi baslatildi. Cikmak icin 'q' tusuna basin.")
-
+  
 while True:
     ret, frame = cap.read()
     if not ret:
